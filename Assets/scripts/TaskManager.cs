184ac9b2bb1482a8477a420609a6c88ec777505e
Marcos Garcia <jr329garcia@gmail.com>
2026-04-19 01:57:31 -0700

    completed customizing tasks

--- Assets/scripts/TaskManager.cs
+++ Assets/scripts/TaskManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TaskManager : MonoBehaviour
 {
@@ -20,6 +21,7 @@ public class TaskManager : MonoBehaviour
         view = parent.GetComponent<RectTransform>();
         
         add_bt.onClick.AddListener(AddTask);
+        done_button.onClick.AddListener(CompletePrompt);
         new_position = new Vector3(100,-40,0);
     }
 
@@ -32,16 +34,54 @@ public class TaskManager : MonoBehaviour
     private int button_count = 0;
     void AddTask()
     {
-        GameObject new_task = Instantiate(t);
-        new_task.transform.SetParent(parent.transform);
-        new_task.transform.localPosition = new_position;
-        new_position -= new Vector3(0, 50, 0);
-        but_transf.localPosition -= new Vector3(0, 50 ,0);
+        PromptTask();
         button_count++;
 
         if (button_count >= 9) {
             add_button.SetActive(false);
         }
+    }
+
+    void CompleteCreate(string name, int pomos)
+    {
+        const int SPACING = 60;
+
+        GameObject new_task = Instantiate(t);
+        new_task.transform.SetParent(parent.transform);
+        new_task.transform.localPosition = new_position;
+        new_position -= new Vector3(0, SPACING, 0);
+        but_transf.localPosition -= new Vector3(0, SPACING ,0);
+        new_task.transform.Find("TaskName").gameObject.GetComponent<TMP_Text>().text = name;
+        new_task.transform.Find("Pomos").gameObject.GetComponent<TMP_Text>().text = "Pomo's to complete: " + pomos.ToString();
+    }
+
+    [Header("Input Items")]
+    public GameObject prompt_ui;
+    public TMP_InputField task_name;
+    public TMP_InputField pomo_task;
+    public Button done_button;
+    
+    void PromptTask()
+    {
+        prompt_ui.SetActive(true);
+    }
+
+    void CompletePrompt()
+    {
+        string name = task_name.text;
+        int pomos = 1;
+        if (int.TryParse(pomo_task.text, out int res))
+        {
+            pomos = res;
+        } else
+        {
+            pomos = 1;
+        }
+
+        task_name.text = "";
+        pomo_task.text = "";
+        CompleteCreate(name, pomos);
+        prompt_ui.SetActive(false);
     }
     
 }
